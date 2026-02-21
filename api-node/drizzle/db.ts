c80a2f6e6d58988922f85754f4d51dec84e3a9a3
Gawc1uuu <gawlikjakub9@gmail.com>
2026-02-21 20:44:26 +0100

    fix

--- api-node/drizzle/db.ts
+++ api-node/drizzle/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import fs from 'fs';
 
-let databaseUrl = process.env.DATABASE_URL || fs.readFileSync(process.env.DATABASE_URL_FILE!, 'utf8')
+let databaseUrl = process.env.DATABASE_URL || fs.readFileSync(process.env.DATABASE_URL_FILE!, 'utf8').trim()
 
 export const pool = new Pool({
     connectionString: databaseUrl,
