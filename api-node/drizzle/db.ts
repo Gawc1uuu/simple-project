b12b8be05bd0f5dfa97ab7cf4b6cc69f1af55ed2
Gawc1uuu <gawlikjakub9@gmail.com>
2026-02-21 20:33:35 +0100

    introducing secrets

--- api-node/drizzle/db.ts
+++ api-node/drizzle/db.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
+import fs from 'fs';
+
+let databaseUrl = process.env.DATABASE_URL || fs.readFileSync(process.env.DATABASE_URL_FILE!, 'utf8')
 
 export const pool = new Pool({
-    connectionString: process.env.DATABASE_URL!,
+    connectionString: databaseUrl,
   });
 
 export const db = drizzle({ client: pool });
